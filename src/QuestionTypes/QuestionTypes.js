d2cb21a9181b5a3385637839e566e83ee398a635
halfpasta <christian.george360@gmail.com>
2021-10-05 00:02:53 -0400

    this component will need to render each type of question that can possibly be added to survey

---
+++ src/QuestionTypes/QuestionTypes.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+
+const QuestionTypes = (qType) => {
+    const questionType = () => {
+        switch(qType.qType) {
+            case 'multiple-choice':
+            return (<div>multiple-choice</div>);
+            case 'dropdown':
+            return (<div>dropdown</div>);
+            case 'checkboxes':
+            return (<div>checkboxes</div>);
+            case 'star-rating':
+            return (<div>star-rating</div>);
+            case 'ranking':
+            return (<div>ranking</div>);
+            case 'file-upload':
+            return (<div>file-upload</div>);
+            case 'slider':
+            return (<div>slider</div>);
+            case 'single-textbox':
+            return (<div>single-textbox</div>);
+            case 'multiple-textbox':
+            return (<div>multiple-textbox</div>);
+            case 'comment-box':
+            return (<div>comment-box</div>);
+            case 'contact-information':
+            return (<div>contact-information</div>);
+            case 'matrix-of-dropdowns':
+            return (<div>matrix-of-dropdowns</div>);
+            case 'date-time':
+            return (<div>date-time</div>);
+            default:
+            return (<div>default</div>)
+         }
+    }
+
+    return <div className='question-type-wrapper wrapper'>{questionType()}</div>
+}
+
+export default QuestionTypes;
