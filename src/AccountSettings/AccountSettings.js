35d4b9cd6df51a13876adc1be5d4441581ec4873
halfpasta <christian.george360@gmail.com>
2021-10-08 12:56:29 -0400

    imported nav

--- src/AccountSettings/AccountSettings.js
+++ src/AccountSettings/AccountSettings.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import Navigation from '../Navigation/Navigation';
 export default function AccountSettings() {
     return (
         <div>
+            <Navigation />
             This is AccountSettings component
         </div>
     )
