d528dfb7aaf05bedc964bb253b40831f9ac67eba
halfpasta <christian.george360@gmail.com>
2021-10-08 12:57:48 -0400

    took mysurveys link out while building mysurveys component

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import MySurveys from '../MySurveys/MySurveys.js'
+import MySurveys from '../MySurveys/MySurveys.js';
 
 export default function Navigation() {
-    return <div>
+    return <div className='nav-wrapper' style={{display: 'flex', width:'100%', justifyContent: 'space-evenly', margin: 15}}>
         <Link to='/account-settings'>Account Settings</Link>
-        <MySurveys>My Surveys</MySurveys>
        <button>Log Out</button>
     </div>
 }
