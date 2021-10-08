548b037ffb1bfd9d2b70ecb26f2103b7f3ce6c82
halfpasta <christian.george360@gmail.com>
2021-10-08 12:55:27 -0400

    scaffold for component to take surveys

---
+++ src/SurveyTaker/SurveyTaker.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SurveyTaker = () => {
+    // This component will make a get call to the server and render a Survey in a way that
+    // the user can take it easily
+    return <div>THIS NOT DONE YET LOL</div>
+}
