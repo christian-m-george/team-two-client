2d97234cb30084ed4b6d5791d6e5c0bd0230e80c
halfpasta <christian.george360@gmail.com>
2021-10-12 00:34:31 -0400

    wrapper makes call to api based on survey ID

--- src/SurveyTaker/SurveyTakerWrapper.js
+++ src/SurveyTaker/SurveyTakerWrapper.js
@@ -1,68 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SurveyTaker from './SurveyTaker';
 
 const SurveyTakeWrapper = () => {
+    const [survey, setSurvey] = useState(null);
+    const [questions, setQuestions] = useState(null);
     const surveyId = 86;
+    
+    const questionUrl = `http://localhost:8000/question/${surveyId}`;
+    const surveyUrl = `http://localhost:8000/survey/${surveyId}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      credentials: 'include',
+      cors: true
+    };
 
     const getQuestions = async () => {
-        const url = `http://localhost:8000/question/${surveyId}`;
-        const options = {
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json;charset=UTF-8',
-            // 'Access-Control-Allow-Credentials': true
-          },
-          credentials: 'include',
-          cors: true
-        };
-    
-        const response = await fetch(url, options)
-          .then(res => {
+        const response = await fetch(questionUrl, options)
+          .then(async res => {
+            console.log(res.status);
             if(res.status === 200) {
-                return res.json().then(data => data)
+                return res.json().then(data =>  data).catch(error => console.log(error))
+            } else {
+              console.log("getSurvey returned a " + res.status);
             }
           }).catch(err => {
               console.error(err);
           });
+          if(response) setQuestions(response);
         return response;
     }
 
     const getSurvey = async () => {
-        const url = `http://localhost:8000/survey/${surveyId}`;
-        const options = {
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json;charset=UTF-8',
-            // 'Access-Control-Allow-Credentials': true
-          },
-          credentials: 'include',
-          cors: true
-        };
-    
-        const response = await fetch(url, options)
+        const response = await fetch(surveyUrl, options)
           .then(res => {
             if(res.status === 200) {
-                return res.json().then(data => data)
+              return res.json().then(data => data).catch(error => console.log(error))
             }
+            else(console.log("getSurvey returned a " + res.status))
           }).catch(err => {
               console.error(err);
           });
+          if(response) setSurvey(response);
         return response;
     }
 
+
+
     return (
-
-            <div style={{width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                <button 
-                    style={{width: 40, height: 20, padding: 20, margin: 5}}
-                    onClick={() => {
-                    getQuestions().then(data => console.log(data)).then(
-                    getSurvey().then(data => console.log(data)).catch(err => console.log(err)));
-                }}>Click on me</button>
-            </div>
-
+      <div>
+        <div style={{width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <button 
+                style={{width: 40, height: 20, padding: 20, margin: 5}}
+                onClick={() => getQuestions().then(data => {
+                    console.log(data);
+                    getSurvey().then(values => console.log(values)).catch(err => console.log(err))
+                }
+                )}>Click on me</button>
+        </div>
+        {(survey && questions ? <SurveyTaker survey={survey} questions={questions}></SurveyTaker> : <div>nothin yet</div> )}
+      </div>
     )
 }
+
+
+
+
+
+
 
 export default SurveyTakeWrapper;
