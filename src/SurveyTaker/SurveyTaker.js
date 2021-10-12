8cb25c0b0dd614217a05b264d38e70b7ba702852
halfpasta <christian.george360@gmail.com>
2021-10-12 00:34:45 -0400

    survey taker renders data about the survey

--- src/SurveyTaker/SurveyTaker.js
+++ src/SurveyTaker/SurveyTaker.js
@@ -1,8 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 const SurveyTaker = (props) => {
-    const [survey, setSurvey] = useState();
-    
 
-    return <div>THIS NOT DONE YET LOL</div>
+        return <div>
+            <div>{props.survey.title} + this is survey title</div>
+                <div>{props.survey.createdAt}</div>
+                <ul style={{width: '100%', paddingInlineStart: 0, listStyle:'none', display: "flex-column", justifyContent: 'center', alignItems: 'center'}}>{(props.questions.length > 0) ?  props.questions.map(item => <li>
+                    <div>{item.questionType}</div>
+                    <div>{item.num}</div>
+                    <div>{item.questionText}</div>
+                    <div>
+                        <ul>
+                            {(item.answers.length > 0) ? item.answers.map((answer) => <li key={item.num} style={{listStyle: "none", display: "flex-row", justifyContent: 'center', alignItems: 'center'}}>{answer}<input name={`${item.num}`} type='radio'/></li>) : null}
+                        </ul>
+                    </div>
+            </li>): null}</ul>
+        </div>
 }
+
+export default SurveyTaker;
