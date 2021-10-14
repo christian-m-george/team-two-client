6bf51677f939282c28acd3bf5b29fe5ae37266fa
halfpasta <christian.george360@gmail.com>
2021-10-14 16:57:57 -0400

    change to mysurvey component request url

--- src/MySurveys/MySurveys.js
+++ src/MySurveys/MySurveys.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
 
 const MySurveys = () => {
-  const [surveys, setSurveys] = useState([]);
-  const url = process.env.NODE_ENV === 'production' ? `${process.env.REACT_APP_HEROKU}/survey/86` : `${process.env.REACT_APP_LOCAL}/survey/86`;
+  const [surveys, setSurveys] = useState(["", ""]);
+  const url = process.env.NODE_ENV === 'production' ? `${process.env.REACT_APP_HEROKU}/survey/all` : `${process.env.REACT_APP_LOCAL}/survey/all`;
   const options = {
     method: 'GET',
     headers: {
@@ -12,11 +12,13 @@ const MySurveys = () => {
     credentials: 'include',
     cors: true
   };
+
   
   useEffect(() => {
     fetch(url, options)
       .then(results => results.json())
       .then(data => {
+        console.log(data)
         const surveys = data;
         setSurveys(surveys);
       });
@@ -25,9 +27,9 @@ const MySurveys = () => {
   return (
     <div style={{width: '100%', justifyContent: 'center', alignItems: 'center'}}>
       <div style={{display: "flex", justifyContent: "center", alignItems: 'center'}}><h1>My Surveys</h1></div>
-     <ul style={{height: 1000, width: "100%", listStyle: "none", paddingInlineStart: 0}}>{surveys.length > 0 ? surveys.map(item => <li key={item.id}
+     <ul style={{height: 1000, width: "100%", listStyle: "none", paddingInlineStart: 0}}>{console.log(typeof {surveys} + " THIS IS TYPE")}{surveys.length == 0 ?  <div>...Loading</div> : surveys.map(item => <li key={item.id}
      style={{padding: 10, margin: "auto", width: "85%", fontSize: 8}}
-     >{JSON.stringify(item)}</li>) : <div>...Loading</div>}</ul>
+     >{JSON.stringify(item)}</li>)}</ul>
     </div>
   );
 }
