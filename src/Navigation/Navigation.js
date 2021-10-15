a97b529a724ed27e978a6704821ebedc2b971b8d
halfpasta <christian.george360@gmail.com>
2021-10-15 16:44:12 -0400

    added home link to nav

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -4,8 +4,8 @@ import Logout from '../Logout/Logout.js';
 
 export default function Navigation() {
     return <div className='nav-wrapper' style={{display: 'flex', width:'100%', justifyContent: 'space-evenly', margin: 15}}>
-
         <Link to='/account-settings'>Account Settings</Link>
+        <Link to='/'>Home</Link>
        <Logout />
     </div>
 }
