0dae0ef805d0266fa4dfa4d7fe2651fb0c6da9b4
halfpasta <christian.george360@gmail.com>
2021-10-18 14:07:14 -0400

    nothin changed lol

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navigation from '../Navigation/Navigation';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useHistory } from 'react-router-dom';
 
 
 export default function Home() {
+    // const history = useHistory();
+    // const token = history.location.state?.token;
+
+    // const url = process.env.NODE_ENV === 'production' ? `${process.env.REACT_APP_HEROKU}` : `${process.env.REACT_APP_LOCAL}`;
+    // const validateTokenOptions = {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json;charset=UTF-8',
+    //   },
+    //   body: JSON.stringify({
+    //       token: token
+    //   }),
+    //   credentials: 'include',
+    //   cors: true
+    // };
+
+    // useEffect(() => {
+    //     fetch(`${url}/user/check-token`, validateTokenOptions)
+    //       .then(res => 
+    //         {
+    //           if (res.status === 200) {
+    //             res.json()
+    //           } else {
+    //             history.push('/login')
+    //           }
+    //         })
+    //   }, []);
+
+
     return <div className='home'>
         <div className='wrapper'>
             <Navigation></Navigation>
