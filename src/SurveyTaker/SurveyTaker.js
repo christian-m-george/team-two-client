18ec4c4615bbdb4dbeebf260dec7a12db5b47a90
halfpasta <christian.george360@gmail.com>
2021-10-17 23:22:26 -0400

    renders survey poorly

--- src/SurveyTaker/SurveyTaker.js
+++ src/SurveyTaker/SurveyTaker.js
@@ -1,21 +1,89 @@
-import React from 'react';
+import { loadPartialConfig } from '@babel/core';
+import React,  { useEffect, useState } from 'react';
+import { useHistory , useParams} from 'react-router';
+import Navigation from '../Navigation/Navigation';
 
-const SurveyTaker = (props) => {
+const SurveyTaker = () => {
+    const params = useParams();
+    const history = useHistory();
+    const [userId, setUserId] = useState(null);
+    const [emails, setEmails] = useState([]);
+    const [survey, setSurvey] = useState(null);
+    const [questions, setQuestions] = useState(null);
+    const token  = params.token;
+    const [checkedToken, setCheckedToken] = useState(false);
+    const [error, setError] = useState(null);
 
-        return <div>
-            <div>{props.survey.title} + this is survey title</div>
-                <div>{props.survey.createdAt}</div>
-                <ul style={{width: '100%', paddingInlineStart: 0, listStyle:'none', display: "flex-column", justifyContent: 'center', alignItems: 'center'}}>{(props.questions.length > 0) ?  props.questions.map(item => <li>
-                    <div>{item.questionType}</div>
-                    <div>{item.num}</div>
-                    <div>{item.questionText}</div>
-                    <div>
-                        <ul>
-                            {(item.answers.length > 0) ? item.answers.map((answer) => <li key={item.num} style={{listStyle: "none", display: "flex-row", justifyContent: 'center', alignItems: 'center'}}>{answer}<input name={`${item.num}`} type='radio'/></li>) : null}
-                        </ul>
-                    </div>
-            </li>): null}</ul>
+    
+    const url = process.env.NODE_ENV === 'production' ? `${process.env.REACT_APP_HEROKU}` : `${process.env.REACT_APP_LOCAL}`;
+    const validateTokenOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      body: JSON.stringify({
+          token: token
+      }),
+      credentials: 'include',
+      cors: true
+    };
+
+    useEffect(() => {
+        fetch(`${url}/survey/verify`, validateTokenOptions)
+          .then(res => 
+            {
+              if (res.status === 200) {
+                res.json().then(data => {
+                  console.log(data);
+                  setEmails(data.emails);
+                  setSurvey(data.survey);
+                  setQuestions(data.questions);
+                  setCheckedToken(true);
+                })
+              } else {
+                setError('The token was not verified')
+              }
+            })
+      }, []);
+
+
+    return (
+    <div style={{width: "100%"}}>
+        <Navigation />
+        {token && !checkedToken ? <div>checking token...</div> : null}
+        {error ? <div>{error}</div> : null}
+
+        <form>
+        <div style={{width:'100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <h1>{survey ? `${survey.title}` : "...loading"}</h1>
         </div>
+        <ul style={{margin: 'auto', listStyle: 'none', paddingInlineStart: 0, width:'100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        {questions ? questions.map((question) => <li key={question.order} style={{padding: 20, paddingInlineStart: 0, width:'100%', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <div style={{justifyContent: 'left', width: "85%"}}>
+                  <h5>{question.order}. {question.questionText}</h5>
+                </div>
+
+                <ul style={{width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: 'auto'}}>
+                {question.answers.map((answer, idx) => <li key={idx} style={{width:'100%', paddingInlineStart: 0, display: 'flex', justifyContent: 'left', alignItems: 'center'}}>
+                    <p style={{ margin: 5}}>{answer}</p>
+                    <input 
+                    type='radio' 
+                    name={`options${questions.order}`} 
+                    onClick={(event) => {
+                      event.preventDefault();
+                      console.log('clicked'
+                    )}}
+                    name={`${question.order}`} />
+                </li>
+                )}
+                </ul>
+            </li>
+        ) : null}
+        </ul>
+        </form>
+    </div>
+    )
 }
 
 export default SurveyTaker;
